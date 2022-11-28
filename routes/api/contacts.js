af1743d41e626b53ee38fcd611b59acf01537462
SeRgNeschetniy <xdsergx@gmail.com>
2022-11-28 23:23:47 +0200

    add controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,75 +1,22 @@
 const express = require("express");
 const {
+  getContacts,
+  getContactnbyId,
+  postContact,
+  deleteContact,
+  putContact,
+} = require("../../controllers/contactsController");
+const {
   addContactsValidation,
   putContactsValidation,
 } = require("../../middlewares/validationMidlewares");
 
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../../models/contacts");
-
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const data = await listContacts();
-    res.status(200).json(data);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const data = await getContactById(contactId);
-    if (!data) {
-      return res.status(400).json({ message: "Not found" });
-    }
-    return res.status(200).json(data);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post("/", addContactsValidation, async (req, res, next) => {
-  try {
-    const data = await addContact(req.body);
-    return res.status(201).json(data);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const data = await removeContact(contactId);
-    if (data) {
-      return res.status(200).json({ message: "contact deleted" });
-    }
-    res.status(400).json({ message: "Not found" });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", putContactsValidation, async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-
-    const data = updateContact(contactId, req.body);
-    if (data === -1) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    return res.status(200).json(data);
-  } catch (erorr) {
-    next(erorr);
-  }
-});
+router.get("/", getContacts);
+router.get("/:contactId", getContactnbyId);
+router.post("/", addContactsValidation, postContact);
+router.delete("/:contactId", deleteContact);
+router.put("/:contactId", putContactsValidation, putContact);
 
 module.exports = router;
