d9b427b717f933ca43b38e09377338bfe0109e86
SeRgNeschetniy <xdsergx@gmail.com>
2022-12-05 17:00:58 +0200

    add middlewares

--- middlewares/validationMidlewares.js
+++ middlewares/validationMidlewares.js
@@ -1,11 +1,10 @@
+const RequestError = require("../helpers/requestError");
+
 const validator = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      error.status = 400;
-      error.message = "missing required name field";
-      next(error);
-      return;
+      next(RequestError(400, error.message));
     }
 
     next();
---
+++ models/contact.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const handleSchemaErrors = require("../middlewares/handleSchemaErrors");
+
+const addContactSchema = Joi.object({
+  name: Joi.string().min(2).max(30).required(),
+  email: Joi.string().email(),
+  phone: Joi.string().min(10).max(14).required(),
+  favorite: {
+    type: Joi.bool(),
+  },
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: {
+    type: Joi.bool().required(),
+  },
+});
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      require: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleSchemaErrors);
+
+const schemas = {
+  addContactSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, schemas };
