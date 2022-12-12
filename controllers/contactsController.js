7f4a1474de57fbe0c7ed6aef26e6f80eafcee272
SeRgNeschetniy <xdsergx@gmail.com>
2022-12-12 21:53:36 +0200

    add hw

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,18 +1,26 @@
 const { Contact } = require("../models/contact");
 
-const getContacts = async (_, res) => {
-  const data = await Contact.find();
+const getContacts = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const data = await Contact.find({ owner }, "", {
+    skip: skip,
+    limit: limit,
+  });
   res.status(200).json(data);
 };
 
 const addContact = async (req, res) => {
-  const data = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const data = await Contact.create({ ...req.body, owner });
   return res.status(201).json(data);
 };
 
 const getContactById = async (req, res) => {
-  const { contactId } = req.params;
-  const data = await Contact.findById(contactId);
+  const { contactId: _id } = req.params;
+  const { _id: owner } = req.user;
+  const data = await Contact.findOne({ _id, owner });
   if (!data) {
     return res.status(400).json({ message: "Not found" });
   }
@@ -20,8 +28,9 @@ const getContactById = async (req, res) => {
 };
 
 const updateContactById = async (req, res) => {
-  const { contactId } = req.params;
-  const data = await Contact.findByIdAndUpdate(contactId, req.body, {
+  const { contactId: _id } = req.params;
+  const { _id: owner } = req.user;
+  const data = await Contact.findByIdAndUpdate({ _id, owner }, req.body, {
     new: true,
   });
   if (!data) {
@@ -31,8 +40,9 @@ const updateContactById = async (req, res) => {
 };
 
 const updateContactFavorite = async (req, res) => {
-  const { contactId } = req.params;
-  const data = await Contact.findByIdAndUpdate(contactId, req.body, {
+  const { contactId: _id } = req.params;
+  const { _id: owner } = req.user;
+  const data = await Contact.findByIdAndUpdate({ _id, owner }, req.body, {
     new: true,
   });
   if (!data) {
@@ -42,8 +52,9 @@ const updateContactFavorite = async (req, res) => {
 };
 
 const deleteContact = async (req, res) => {
-  const { contactId } = req.params;
-  const data = await Contact.findByIdAndRemove(contactId);
+  const { contactId: _id } = req.params;
+  const { _id: owner } = req.user;
+  const data = await Contact.findByIdAndRemove({ _id, owner });
   if (data) {
     return res.status(200).json({ message: "contact deleted" });
   }
---
+++ models/users.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const handleSchemaErrors = require("../middlewares/handleSchemaErrors");
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+userSchema.post("save", handleSchemaErrors);
+
+const signupSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const subscriptionSchema = Joi.object({
+  subscription: Joi.string().valid("starter", "pro", "business").required(),
+});
+
+const schemas = {
+  signupSchema,
+  loginSchema,
+  subscriptionSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemas };
